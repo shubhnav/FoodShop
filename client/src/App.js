78edfd2f6bf375014783c288f79a95dbf42bfbe8
shubhnav <shubhanshuprajapati18@gmail.com>
2020-11-08 17:28:40 +0530

    freecharge6

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-
+import { Card, Button } from 'react-bootstrap';
 class App extends Component {
   constructor(props){
     super(props);
   }
   render(){
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top"  />
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-  </Card>
+     <button type="button" onClick = {this.handleOnClick}>Click Me!</button>
   )}
 
-  async UNSAFE_componentWillMount(){
+  async handleOnClick(){
     return new Promise(async(resolve,reject)=>{
       await fetch("/hello",{
           mode: 'cors',
@@ -31,7 +21,7 @@ class App extends Component {
           let data = await response.json();
           return data;
         }).then(data=>{
-          console.log("response",data);
+
         })
     })
     }
