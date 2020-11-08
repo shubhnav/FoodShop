3ce80c2adc72b6e588f035e0f78a47d816c47817
shubhnav <shubhanshuprajapati18@gmail.com>
2020-11-08 18:20:12 +0530

    freecharge12

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
       cards : [],
       data: false
     }
+    this.handleOnClick = this.handleOnClick.bind(this)
   }
   render(){
     if(this.state.data === true){
