3739678ff736f298d05c1f6c2fb1f65e148f833f
Shubhanshu Prajapati <shubh@Shubhanshus-MacBook-Air.local>
2025-09-26 09:40:48 +0530

    deploy using github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import packageJson from "./package.json" with { type: "json" }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/FoodShop/',
   server: {
     open: true,
     proxy: {
